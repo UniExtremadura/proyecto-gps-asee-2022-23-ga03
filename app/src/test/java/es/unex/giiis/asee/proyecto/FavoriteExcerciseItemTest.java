2a7b8c03f88fa9eb058f25061625a08b9e13729c
GA03Marron <jguillenw@alumnos.unex.es>
2022-11-30 11:04:39 +0100

    PGA03-328 CU03: IMP_Test Components_DJ se ha realizado la implementación de las pruebas unitarias en la clase FavoriteExcerciseItem #DONE

---
+++ app/src/test/java/es/unex/giiis/asee/proyecto/FavoriteExcerciseItemTest.java
@@ -0,0 +1,192 @@
+package es.unex.giiis.asee.proyecto;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import android.content.Intent;
+
+import androidx.room.Ignore;
+
+import es.unex.giiis.asee.proyecto.ui.ejercicios.FavoriteExcerciseItem;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FavoriteExcerciseItemTest {
+
+    private FavoriteExcerciseItem item;
+
+    @Mock
+    private Intent intent;
+
+    @Before
+    public void init() {
+        item = new FavoriteExcerciseItem(1, "press de banca", "resistencia", "pechito", "barra", "mu dura mu dura", "Tienes que ser masoca para meter este ejercicio en favoritos", 7);
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void FavoriteExcerciseItem1() {
+        FavoriteExcerciseItem favoriteExcerciseItem = new FavoriteExcerciseItem("pesas", "fuerza", "biceps", "mancuerna", "easy remix", "se puede se puede", 17);
+        assertEquals("pesas", favoriteExcerciseItem.getTittle());
+        assertEquals("fuerza", favoriteExcerciseItem.getType());
+        assertEquals("biceps", favoriteExcerciseItem.getMuscle());
+        assertEquals("mancuerna", favoriteExcerciseItem.getEquipment());
+        assertEquals("easy remix", favoriteExcerciseItem.getDifficulty());
+        assertEquals("se puede se puede", favoriteExcerciseItem.getInstructions());
+        assertEquals(17, favoriteExcerciseItem.getUserid());
+
+    }
+
+    @Test
+    public void FavoriteExcerciseItem2() {
+        FavoriteExcerciseItem favoriteExcerciseItem = new FavoriteExcerciseItem(5, "pesas", "fuerza", "biceps", "mancuerna", "easy remix", "se puede se puede", 17);
+        assertEquals("pesas", favoriteExcerciseItem.getTittle());
+        assertEquals("fuerza", favoriteExcerciseItem.getType());
+        assertEquals("biceps", favoriteExcerciseItem.getMuscle());
+        assertEquals("mancuerna", favoriteExcerciseItem.getEquipment());
+        assertEquals("easy remix", favoriteExcerciseItem.getDifficulty());
+        assertEquals("se puede se puede", favoriteExcerciseItem.getInstructions());
+        assertEquals(17, favoriteExcerciseItem.getUserid());
+        assertEquals(5, favoriteExcerciseItem.getId());
+
+
+    }
+
+    @Test
+    public void FavoriteExcerciseItem3() {
+        when(intent.getLongExtra("ID", 0)).thenReturn(12L);
+        when(intent.getStringExtra("title")).thenReturn("prensa");
+        when(intent.getStringExtra("type")).thenReturn("resistencia");
+        when(intent.getStringExtra("muscle")).thenReturn("pierna");
+        when(intent.getStringExtra("equipment")).thenReturn("maquina de prensa");
+        when(intent.getStringExtra("difficulty")).thenReturn("dificil");
+        when(intent.getStringExtra("instructions")).thenReturn("hasta que te tiemblen las piernas");
+        when(intent.getLongExtra("userid", 0)).thenReturn(4L);
+
+        intent.putExtra("ID",12);
+        intent.putExtra("title","prensa");
+        intent.putExtra("type","resistencia");
+        intent.putExtra("muscle","pierna");
+        intent.putExtra("equipment","maquina de prensa");
+        intent.putExtra("difficulty","dificil");
+        intent.putExtra("instructions", "hasta que te tiemblen las piernas");
+        intent.putExtra("userid", 4);
+
+        FavoriteExcerciseItem favoriteExcerciseItem = new FavoriteExcerciseItem(intent);
+        assertEquals(12, favoriteExcerciseItem.getId());
+        assertEquals("prensa", favoriteExcerciseItem.getTittle());
+        assertEquals("resistencia", favoriteExcerciseItem.getType());
+        assertEquals("pierna", favoriteExcerciseItem.getMuscle());
+        assertEquals("maquina de prensa", favoriteExcerciseItem.getEquipment());
+        assertEquals("dificil", favoriteExcerciseItem.getDifficulty());
+        assertEquals("hasta que te tiemblen las piernas", favoriteExcerciseItem.getInstructions());
+        assertEquals(4, favoriteExcerciseItem.getUserid());
+
+
+
+    }
+
+    @Test
+    public void getId() {
+        assertEquals(1, item.getId());
+    }
+
+    @Test
+    public void setId() {
+        assertEquals(1, item.getId());
+        item.setId(2);
+        assertEquals(2, item.getId());
+    }
+
+    @Test
+    public void getTittle() {
+        assertEquals("press de banca", item.getTittle());
+    }
+
+    @Test
+    public void setTittle() {
+        assertEquals("press de banca", item.getTittle());
+        item.setTittle("banca de press");
+        assertEquals("banca de press", item.getTittle());
+    }
+
+    @Test
+    public void getType() {
+        assertEquals("resistencia", item.getType());
+    }
+
+    @Test
+    public void setType() {
+        assertEquals("resistencia", item.getType());
+        item.setType("fuerza");
+        assertEquals("fuerza", item.getType());
+    }
+
+    @Test
+    public void getMuscle() {
+        assertEquals("pechito", item.getMuscle());
+    }
+
+    @Test
+    public void setMuscle() {
+        assertEquals("pechito", item.getMuscle());
+        item.setMuscle("pecho");
+        assertEquals("pecho", item.getMuscle());
+    }
+
+    @Test
+    public void getEquipment() {
+        assertEquals("barra", item.getEquipment());
+    }
+
+    @Test
+    public void setEquipment() {
+        assertEquals("barra", item.getEquipment());
+        item.setEquipment("nada");
+        assertEquals("nada", item.getEquipment());
+    }
+
+    @Test
+    public void getDifficulty() {
+        assertEquals("mu dura mu dura", item.getDifficulty());
+    }
+
+    @Test
+    public void setDifficulty() {
+        assertEquals("mu dura mu dura", item.getDifficulty());
+        item.setDifficulty("nahh");
+        assertEquals("nahh", item.getDifficulty());
+    }
+
+    @Test
+    public void getInstructions() {
+        assertEquals("Tienes que ser masoca para meter este ejercicio en favoritos", item.getInstructions());
+    }
+
+    @Test
+    public void setInstructions() {
+        assertEquals("Tienes que ser masoca para meter este ejercicio en favoritos", item.getInstructions());
+        item.setInstructions("levanta la barra y asi hasta 100");
+        assertEquals("levanta la barra y asi hasta 100", item.getInstructions());
+    }
+
+    @Test
+    public void getUserid() {
+        assertEquals(7, item.getUserid());
+    }
+
+    @Test
+    public void setUserid() {
+        assertEquals(7, item.getUserid());
+        item.setUserid(9);
+        assertEquals(9, item.getUserid());
+    }
+
+
+}
